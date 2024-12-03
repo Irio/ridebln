2e6ab9b8ac8dfb504ef63c90eb5c1f8def8df680
Irio Musskopf <irio@protonmail.com>
2024-12-03 18:15:47 +0100

    Clean up dependencies

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 from dynaconf import Dynaconf
 
-
 settings = Dynaconf(
     settings_files=["settings.toml"],
 )
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 import re
-import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
